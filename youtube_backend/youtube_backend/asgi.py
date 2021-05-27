bf6c756bd0900d3bc6a10ede74da17ad5ff95c2b
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-27 13:56:14 -0500

    refactored directory structure and names

--- youtube_backend/youtube_backend/asgi.py
+++ youtube_backend/youtube_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Youtube_Clone project.
+ASGI config for youtube_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Youtube_Clone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_backend.settings')
 
 application = get_asgi_application()
