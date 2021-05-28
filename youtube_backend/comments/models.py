2ba39773c8ca8d4591a4f764e8b2c713c2999978
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-28 08:58:39 -0500

    modified models.py for video_id, content, and replies

--- youtube_backend/comments/models.py
+++ youtube_backend/comments/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class Comment(models.Model):
     date = models.DateField('%m/%d/%Y')
     like_count = models.IntegerField(default=0)
-    video_id = ''
-    content = []
-    replies = []
+    video_id = models.CharField(max_length=20, null=True, blank=True)
+    content = models.CharField(max_length=250, null=True, blank=True)
+    replies = models.CharField(max_length=50, null=True, blank=True)
